7835afdd8a56cc7af3f3a04ad40ac8e3e245f8f3
benjamibono <benjamibono@gmail.com>
2025-05-02 17:57:34 +0200

    Add Vercel analytics and speed insights to layout
    Incorporate @vercel/analytics and @vercel/speed-insights into the project by updating package.json and package-lock.json. Integrate SpeedInsights and Analytics components into the RootLayout for enhanced performance tracking and analytics capabilities.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./components/Providers";
 import { PreloadCommonData } from "./components/PreloadCommonData";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +32,8 @@ export default function RootLayout({
         <Providers>
           <PreloadCommonData />
           {children}
+          <SpeedInsights />
+          <Analytics />
         </Providers>
       </body>
     </html>
